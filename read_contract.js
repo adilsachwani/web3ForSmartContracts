495072f95af4b2cc8cb5d8bb8d5ac462c18573db
adilsachwani <adilsachwani@gmail.com>
2019-04-06 10:11:03 +0500

    contract changed

--- read_contract.js
+++ read_contract.js
@@ -6,9 +6,9 @@ const web3 = new Web3('https://ropsten.infura.io/v3/2fd01be421ed440d864daa735384
 const account = '0x5A51Ee918d1f0c657e1bF822eFD08b4F4325172f';
 const privateKey = Buffer.from('03307208505F41520C77DE84E98CC4E4E449C6371AFD458F12F1432A5404A8B5','hex');
 
-const contractABI = [{"constant":true,"inputs":[],"name":"total_posts","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_date","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_id","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_duration","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_secret_key","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"total_voters","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_time","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}];
-const contractAddress = '0x8c41ccec00dcf9f49094bc881be3bf22c37fec84';
+const contractABI = [{"constant":true,"inputs":[],"name":"totalVotes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"castVote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
+const contractAddress = '0xd546baf0dacac8fd193a622bda32749b2df47352';
 
-var contract = new web3.eth.Contract(contractABI, contractAddress);
+const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-contract.methods.election_duration().call({from: account}, (err,res) => { console.log(res); } )
+contract.methods.totalVotes().call({from: account}, (err,res) => { console.log(res); } )
